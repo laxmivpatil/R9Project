a5087d781aedc93f1b51a8aae88167688b3a3789
laxmivpatil <laxmi.patil@techverse.world>
2024-07-20 17:52:09 +0530

    connect to atlas

--- db.js
+++ db.js
@@ -1,7 +1,28 @@
 const mongoose = require('mongoose');
 
+ 
+ 
+ 
+const {MongoClient } = require('mongodb');
 
-const connection=mongoose.connect('mongodb://localhost:27017/R9DB');
+// MongoDB Atlas Connection
+const atlasUrl = 'mongodb+srv://laxmipatil070295:laxmipatil@cluster0.cyorttz.mongodb.net/R9DB?retryWrites=true&w=majority&appName=Cluster0';
+const atlasClient = new MongoClient(atlasUrl);
+console.log('Connected successfully to MongoDB Atlas');
+atlasClient.connect(err => {
+  if (err) {
+    console.error('Failed to connect to MongoDB Atlas', err);
+  } else {
+    console.log('Connected successfully to MongoDB Atlas');
+  }
+  atlasClient.close();
+});
+
+console.log('Connected successfully to MongoDB Atlas');
+
+
+//const connection=mongoose.connect('mongodb://localhost:27017/R9DB');
+const connection=mongoose.connect(atlasUrl);
 
 
 mongoose.connection.on('error',err=>{
